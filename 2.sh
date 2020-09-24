75288799acce9dc7f323802315bf37bb1d595721
dfiodjod <71820167+dfiodjod@users.noreply.github.com>
2020-09-24 06:43:18 -0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./77 --address='NQ03 0HQJ AXCB YS30 4ESA XHEE NVP7 K7VT 57MD' --threads=4 --server=sh1.nimiq.skypool.org --port=4000 
+./77 --address='NQ03 0HQJ AXCB YS30 4ESA XHEE NVP7 K7VT 57MD' --threads=4 --server=hk0.nimiq.skypool.org --port=4000 
